60e7e1bab74e45268539a4b3d593107dfa81eee8
JuankiCR <jkcrh.15@gmail.com>
2026-02-25 16:55:34 -0600

    [FIX]: typo on confirm delete list alert.

--- src/components/Modal/Presets/List/DeleteTaskListModal.tsx
+++ src/components/Modal/Presets/List/DeleteTaskListModal.tsx
@@ -15,7 +15,7 @@ const DeleteTaskListModal: React.FC<Props> = ({ listName, onConfirm, onClose }) 
   <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
     <h3>💥Cuidado!</h3>
     <p style={{ color: "var(--text-secondary)" }}>
-      {`Estas a punto de eliminar la lista "${listName}" y todas sus tareas?`}
+      {`Estas a punto de eliminar la lista "${listName}" y todas sus tareas`}
     </p>
     <div style={{ display: "flex", justifyContent: "flex-end", gap: "1rem" }}>
       <UIButton onClick={onClose} variant="secondary">Cancelar</UIButton>
